10dceea73b3b80e605a98c942172ba0f6984f584
Alex <collo@DESKTOP-0MTRO7E>
2022-09-24 00:55:13 +0200

    design: footer + img wrong import method fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navigation } from './layout/navigation/Navigation';
+import { Navigation as NavComponent } from './components/navigation/Navigation';
 import { BrowserRouter } from 'react-router-dom';
+import { Footer } from './layout/footer/Footer';
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        
+        <NavComponent></NavComponent>
       </header>
       <main>
         <Navigation />
       </main>
+      <Footer></Footer>
     </div>
     </BrowserRouter>
     
