48b9cf058ea2aaab4c2c5d26f7eefbdb88efdd4a
Alex <collo@DESKTOP-0MTRO7E>
2022-09-25 23:25:56 +0200

    fix: useSelector instead of lambda getState to handle update

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,11 +1,10 @@
 import { Link, useLocation } from "react-router-dom"
 import logoIcon from "../../assets/img/argentBankLogo.png"
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { removeUser } from "../../store/reducers/user/UserReducer"
-import store from "../../store/store"
 export function Navigation() {
-    const pathname: string = useLocation().pathname
     const dispatch = useDispatch()
+    const user = useSelector((state:any) => state.user)
     return (
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
@@ -17,19 +16,15 @@ export function Navigation() {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link hidden={pathname !== "/" && pathname !== "/login"} className="main-nav-item" to="login">
+                <Link hidden={user.token !== null} className="main-nav-item" to="login">
                     <i className="fa fa-user-circle"></i>
                     Sign In
                 </Link>
-                {/* TODO: Handle redux user */}
-                <Link hidden={pathname !== "/user"} className="main-nav-item" to="user">
+                <Link hidden={user.token === null} className="main-nav-item" to="user">
                     <i className="fa fa-user-circle"></i>
                     Tony
                 </Link>
-                <Link onClick={() => {
-                    dispatch(removeUser())
-                    console.log(store.getState().user)
-                    }} hidden={pathname !== "/user"} className="main-nav-item" to="/">
+                <Link onClick={() => dispatch(removeUser())} hidden={user.token === null} className="main-nav-item" to="/">
                     <i className="fa fa-sign-out"></i>
                     Sign Out
                 </Link>
--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { login } from "../../services/UsersService";
 import "./signIn.css";
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setUser } from "../../store/reducers/user/UserReducer";
 import { DateTime } from "luxon";
 import { useNavigate } from "react-router-dom";
@@ -9,13 +9,16 @@ import store from "../../store/store";
 export function SignIn() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
-    const [form, setForm] = useState({email:"", password:""});
+    const [form, setForm] = useState({ email: "", password: "" });
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    console.log(store.getState().user)
-    if (store.getState().user.token !== null) {
-        navigate("/user")
-    }
+    const user = useSelector((state: any) => state.user)
+    useEffect(() => {
+        if (user.token !== null) {
+            navigate("/user")
+        }
+        
+    }, [])
     return (
         <main className="main bg-dark">
             <section className="sign-in-content">
@@ -25,11 +28,11 @@ export function SignIn() {
                 <form hidden={loading} action="" method="">
                     <div className="input-wrapper">
                         <label htmlFor="username">Username</label>
-                        <input onKeyUp={(e) => setForm({...form,email: e.currentTarget.value})} type="text" id="username" />
+                        <input onKeyUp={(e) => setForm({ ...form, email: e.currentTarget.value })} type="text" id="username" />
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input onKeyUp={(e) => setForm({...form,password: e.currentTarget.value})} type="password" id="password" />
+                        <input onKeyUp={(e) => setForm({ ...form, password: e.currentTarget.value })} type="password" id="password" />
                     </div>
                     <div className="input-remember">
                         <input type="checkbox" id="remember-me" /><label htmlFor="remember-me"
@@ -44,7 +47,7 @@ export function SignIn() {
                             console.log(user)
                             setLoading(false)
                             if (user?.body) {
-                                dispatch(setUser({token: user.body.token, expiresAt:DateTime.now().plus({days: 1}).toMillis()}))
+                                dispatch(setUser({ token: user.body.token, expiresAt: DateTime.now().plus({ days: 1 }).toMillis() }))
                                 navigate("/user")
                             } else {
                                 setError(user?.message)
@@ -62,15 +65,15 @@ export function SignIn() {
 }
 interface Form {
     email: string;
-    password:string;
+    password: string;
 }
 interface FormInfo {
     valid: boolean;
-    error:string;
+    error: string;
 }
-function isFormValid(form:Form):FormInfo {
-    let formInfo : FormInfo = {
-        valid:true,
+function isFormValid(form: Form): FormInfo {
+    let formInfo: FormInfo = {
+        valid: true,
         error: ""
     }
     if (!isEmailValid(form.email)) {
@@ -87,8 +90,8 @@ function isFormValid(form:Form):FormInfo {
 function isEmailValid(email: string) {
     const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     return regex.test(email)
-  }
+}
 
-  function isPasswordValid(password: string) { 
+function isPasswordValid(password: string) {
     return password.length >= 6
-  }
+}
