e777f5ca6af22a9589d4c82b0767d8a989eee090
Alex <collo@DESKTOP-0MTRO7E>
2022-10-07 21:08:07 +0200

    refactoring: user form handler reworked + minors fix css

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -2,7 +2,6 @@ import { DateTime } from "luxon"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import { useSWRConfig } from "swr"
 import { getUserProfile, updateUserProfile } from "../../services/UsersService"
 import { setUser } from "../../store/reducers/user/UserReducer"
 import "./user.css"
@@ -20,47 +19,50 @@ export function User() {
         getUserProfile().then((profile) => {
             if (profile.status === 200) {
                 dispatch(setUser({ ...user, profile: profile.body }))
+                setForm({...form,data: {firstName: profile.body.firstName, lastName: profile.body.lastName} })
             }
         })
 
     }, [])
     const accounts = [{ title: "Argent Bank Checking (x8349)", amount: "$2,082.79", description: "Available Balance" }, { title: "Argent Bank Savings (x6712)", amount: "$10,928.42", description: "Available Balance" }, { title: "Argent Bank Credit Card (x8349)", amount: "$184.30", description: "Current Balance" }]
     const [editMode, setEditMode] = useState(false)
-    const [form, setForm] = useState({ firstName: "", lastName: "" })
-    const [formError, setFormError] = useState("")
+    const [form, setForm] = useState({data: {firstName: "", lastName: ""}, error:"", loading:false })
     if (!user?.profile) return <div>failed to load</div>
     return (
         <main className="main bg-dark">
             <div className="header">
                 <h1 style={{ marginTop: 0, paddingTop: "0.67em" }}>Welcome back<br /><span hidden={editMode}>{user.profile.firstName} {user.profile.lastName}!</span></h1>
-                <div className="ctnInput">
-                    <input className="editProfileNameInput" onChange={(e) => setForm({ ...form, firstName: e.currentTarget.value })} hidden={!editMode} type="text" placeholder="First name" defaultValue={user.profile.firstName} />
-                    <input className="editProfileNameInput" onChange={(e) => setForm({ ...form, lastName: e.currentTarget.value })} hidden={!editMode} type="text" placeholder="Last name" defaultValue={user.profile.lastName} />
+                <div>
+                    <input className="editProfileNameInput" onChange={(e) => setForm({ ...form, data: {...form.data,firstName: e.currentTarget.value} })} hidden={!editMode} type="text" placeholder="First name" value={form.data.firstName} />
+                    <input className="editProfileNameInput" onChange={(e) => setForm({ ...form, data: {...form.data,lastName: e.currentTarget.value} })} hidden={!editMode} type="text" placeholder="Last name" value={form.data.lastName} />
                 </div>
-                <span className="formError" hidden={formError === ""}>{formError}</span>
+                <span className={form.error === "" ? "formError disabled" : "formError"} hidden={form.error === ""}>{form.error}</span>
                 <div className="ctnInput">
                     <button onClick={async () => {
                         if (editMode) {
-                            if (!isFormValid(form.firstName, form.lastName)) {
-                                setFormError("First name and last name must have at least 2 characters")
+                            
+                            if (!isFormValid(form.data.firstName, form.data.lastName)) {
+                                setForm({ ...form, loading: false, error: "First name and last name must have at least 2 characters" })
                                 return
                             }
-                            setFormError("")
-                            const updatedUser = await updateUserProfile(form.firstName, form.lastName)
+                            setForm({ ...form, error: "" })
+                            const updatedUser = await updateUserProfile(form.data.firstName, form.data.lastName)
                             if (updatedUser.body) {
-                                // mutate('http://localhost:3001/api/v1/user/profile')
-                                //TODO HYDRATE
+                                setForm({ ...form, loading: true })
+                                dispatch(setUser({ ...user, profile: updatedUser.body }))
                                 setEditMode(false)
                             } else {
-                                setFormError("Error while updating profile")
+                                setForm({ ...form, loading: false, error: "Error while updating profile" })
                             }
                         } else {
+                            setForm({ ...form, data:{firstName: user.profile.firstName, lastName: user.profile.lastName} })
                             setEditMode(true)
+                            
                         }
                     }} className={editMode ? "edit-button editBtnMode" : "edit-button"}>{editMode ? "Save" : "Edit Name"}</button>
                     <button hidden={!editMode} onClick={() => {
                         //CANCEL
-                        setForm({ firstName: user.profile.firstName, lastName: user.profile.lastName })
+                        setForm({ ...form, data: {firstName: user.profile.firstName, lastName: user.profile.lastName }, error: "" })
                         setEditMode(false)
                     }} className="edit-button editBtnMode">Cancel</button>
                 </div>
